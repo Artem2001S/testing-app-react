0480a14cc114330ef6a2e0dcb6d9707cc93902f2
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-04-07 16:20:24 +0300

    apply saga middleware

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,13 @@
-import { createStore, compose } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from 'redux/rootReducer';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
   compose(
+    applyMiddleware(sagaMiddleware),
     window.__REDUX_DEVTOOLS_EXTENSION__
       ? window.__REDUX_DEVTOOLS_EXTENSION__()
       : (f) => f
