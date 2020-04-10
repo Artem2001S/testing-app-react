623adbaefbbce0aec1bf0e320a85ad16377f4e8e
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-04-10 16:20:55 +0300

    change Button component (add styles for secondary btn)

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import classes from './Button.module.scss';
 
-export default function Button({ children, handleClick }) {
+export default function Button({ children, handleClick, secondary }) {
+  const btnClasses = classNames(classes.Button, {
+    [classes.SecondaryBtn]: secondary,
+  });
+
   return (
-    <button className={classes.Button} onClick={handleClick}>
+    <button className={btnClasses} onClick={handleClick}>
       {children}
     </button>
   );
 }
 
 Button.propTypes = {
+  secondary: PropTypes.bool,
+  children: PropTypes.node,
   handleClick: PropTypes.func,
 };
