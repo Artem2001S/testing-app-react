998cbf905fd61e29c2adeb55c65df37ce6db1a2c
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-04-12 23:04:50 +0300

    convert test item (change created_at property) when getting tests from the server

--- src/redux/api/testOperations.js
+++ src/redux/api/testOperations.js
@@ -4,5 +4,16 @@ import { normalizeTests } from 'redux/normalizr/normalizeTests';
 export async function getTestsFromServer() {
   const response = await instance.get('/tests');
 
-  return normalizeTests(response.data.tests);
+  const tests = response.data.tests.map((test) => {
+    const convertedTest = {
+      ...test,
+      createdAt: new Date(test.created_at).toLocaleString(),
+      createdAtValue: new Date(test.created_at).valueOf(),
+    };
+
+    delete convertedTest.created_at;
+    return convertedTest;
+  });
+
+  return normalizeTests(tests);
 }
