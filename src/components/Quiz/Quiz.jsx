28bbdbc744861272fd3ba9b00e1946fefe280df7
Artem Kazaryan <artem.kazaryan.tema@gmail.com>
2020-04-29 18:15:23 +0300

    move QuestionAnswers to separate component

--- src/components/Quiz/Quiz.jsx
+++ src/components/Quiz/Quiz.jsx
@@ -2,10 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './Quiz.module.scss';
 import QuizQuestion from './QuizQuestion/QuizQuestion';
-import TextInput from 'components/UIElements/TextInput/TextInput';
-import Checkbox from 'components/UIElements/Checkbox/Checkbox';
-import RadioButton from 'components/UIElements/RadioButton/RadioButton';
 import Button from 'components/UIElements/Button/Button';
+import QuizAnswers from './QuizAnswers/QuizAnswers';
 
 export default function Quiz({
   questionTitle,
@@ -29,38 +27,10 @@ export default function Quiz({
         questionsCount={questionsCount}
         currentQuestionNumber={currentQuestionNumber}
       />
-      <div className={classes.Answers}>
-        {answerInputs.map((input, index) => {
-          switch (input.type) {
-            case 'number':
-              return (
-                <TextInput
-                  key={index}
-                  handleChange={inputChangeHandlers[input.name]}
-                  {...input}
-                />
-              );
-            case 'radio':
-              return (
-                <RadioButton
-                  key={index}
-                  handleChange={inputChangeHandlers[input.name]}
-                  {...input}
-                />
-              );
-            case 'checkbox':
-              return (
-                <Checkbox
-                  key={index}
-                  handleChange={inputChangeHandlers[input.name]}
-                  {...input}
-                />
-              );
-            default:
-              return <></>;
-          }
-        })}
-      </div>
+      <QuizAnswers
+        answerInputs={answerInputs}
+        inputChangeHandlers={inputChangeHandlers}
+      />
       <div className={classes.Actions}>
         <Button dangerous handleClick={onFinishQuiz}>
           Finish quiz
@@ -78,6 +48,10 @@ Quiz.propTypes = {
     PropTypes.string,
     PropTypes.number,
   ]),
+  isFinished: PropTypes.bool,
   answerInputs: PropTypes.array.isRequired,
+  correctAnswersCount: PropTypes.number,
+  inputChangeHandlers: PropTypes.object.isRequired,
   onNext: PropTypes.func.isRequired,
+  onFinishQuiz: PropTypes.func.isRequired,
 };
---
+++ src/components/Quiz/QuizAnswers/QuizAnswers.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import TextInput from 'components/UIElements/TextInput/TextInput';
+import RadioButton from 'components/UIElements/RadioButton/RadioButton';
+import Checkbox from 'components/UIElements/Checkbox/Checkbox';
+import classes from './QuizAnswers.module.scss';
+
+export default function QuizAnswers({ answerInputs, inputChangeHandlers }) {
+  return (
+    <div className={classes.Answers}>
+      {answerInputs.map((input, index) => {
+        switch (input.type) {
+          case 'number':
+            return (
+              <TextInput
+                key={index}
+                handleChange={inputChangeHandlers[input.name]}
+                {...input}
+              />
+            );
+          case 'radio':
+            return (
+              <RadioButton
+                key={index}
+                handleChange={inputChangeHandlers[input.name]}
+                {...input}
+              />
+            );
+          case 'checkbox':
+            return (
+              <Checkbox
+                key={index}
+                handleChange={inputChangeHandlers[input.name]}
+                {...input}
+              />
+            );
+          default:
+            return <></>;
+        }
+      })}
+    </div>
+  );
+}
+
+QuizAnswers.propTypes = {
+  answerInputs: PropTypes.array.isRequired,
+  inputChangeHandlers: PropTypes.object.isRequired,
+};
